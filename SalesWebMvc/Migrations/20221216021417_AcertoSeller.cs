e2a574e67fb633d3c877e78baad4e3df72e3a298
Ronny Rocha <ronnysrocha@gmail.com>
2022-12-15 23:29:46 -0300

    Simple Create form

---
+++ SalesWebMvc/Migrations/20221216021417_AcertoSeller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class AcertoSeller : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BirthDater",
+                table: "Seller",
+                newName: "BirthDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BirthDate",
+                table: "Seller",
+                newName: "BirthDater");
+        }
+    }
+}
